7c2b6606aaeaeb60458d44d22fed58160e5c0aa6
Maurice Borgmeier <mborgmeier@tecracer.de>
2020-06-29 19:11:05 +0200

    Add additional integration tests

--- tests/integration/test_bundling.py
+++ tests/integration/test_bundling.py
@@ -4,6 +4,7 @@ Integration tests for the lambda bundler
 NOTE: These download packages and thus require internet access!
 """
 import os
+import pathlib
 import shutil
 import tempfile
 import unittest
@@ -78,3 +79,91 @@ class LayerTestCases(unittest.TestCase):
             self.assertTrue(os.path.exists(
                 os.path.join(assertion_directory, "python", "certifi")
             ))
+
+    def test_build_lambda_package_without_dependencies(self):
+        """Assert a lambda package without dependencies get's put together as expected"""
+
+        with tempfile.TemporaryDirectory() as assertion_directory, \
+            tempfile.TemporaryDirectory() as build_directory, \
+            tempfile.TemporaryDirectory() as source_directory:
+
+            os.environ[BUILD_DIR_ENV] = build_directory
+
+            directories_in_source = ["src/lambda", "tests", "initial"]
+
+            for directory in directories_in_source:
+                pathlib.Path(os.path.join(source_directory, directory)).mkdir(parents=True, exist_ok=True)
+
+            pathlib.Path(os.path.join(source_directory, "initial")).mkdir(parents=True, exist_ok=True)
+            with open(os.path.join(source_directory, "initial", "test.txt"), "w") as handle:
+                handle.write("test-content")
+
+            with open(os.path.join(source_directory, "src", "lambda", "handler.py"), "w") as handle:
+                handle.write("test-content")
+
+            path = build_lambda_package(
+                code_directories=[
+                    os.path.join(source_directory, directory) for directory in directories_in_source
+                ],
+                exclude_patterns=["test.txt"]
+            )
+
+            self.assertTrue(path.endswith(".zip"))
+
+            shutil.unpack_archive(path, assertion_directory)
+
+            # These should be in the zip
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "initial")))
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "lambda", "handler.py")))
+            # This shouldn't be in the zip (exclude list)
+            self.assertFalse(os.path.exists(os.path.join(assertion_directory, "initial", "test.txt")))
+
+    def test_build_lambda_package_with_dependencies(self):
+        """Assert a lambda package with dependencies get's put together as expected"""
+
+        with tempfile.TemporaryDirectory() as assertion_directory, \
+            tempfile.TemporaryDirectory() as build_directory, \
+            tempfile.TemporaryDirectory() as source_directory:
+
+            os.environ[BUILD_DIR_ENV] = build_directory
+
+            directories_in_source = ["src/lambda", "tests", "initial"]
+
+            for directory in directories_in_source:
+                pathlib.Path(os.path.join(source_directory, directory)).mkdir(parents=True, exist_ok=True)
+
+            pathlib.Path(os.path.join(source_directory, "initial")).mkdir(parents=True, exist_ok=True)
+            with open(os.path.join(source_directory, "initial", "test.txt"), "w") as handle:
+                handle.write("test-content")
+
+            with open(os.path.join(source_directory, "src", "lambda", "handler.py"), "w") as handle:
+                handle.write("test-content")
+
+            path_to_requirement_1 = os.path.join(source_directory, "requirements_1.txt")
+            path_to_requirement_2 = os.path.join(source_directory, "requirements_2.txt")
+
+            with open(path_to_requirement_1, "w") as handle_1, \
+                open(path_to_requirement_2, "w") as handle_2:
+
+                handle_1.write("pytz==2020.01")
+                handle_2.write("certifi==2020.6.20")
+
+            path = build_lambda_package(
+                code_directories=[
+                    os.path.join(source_directory, directory) for directory in directories_in_source
+                ],
+                requirement_files=[path_to_requirement_1, path_to_requirement_2],
+                exclude_patterns=["test.txt"]
+            )
+
+            self.assertTrue(path.endswith(".zip"))
+
+            shutil.unpack_archive(path, assertion_directory)
+
+            # These should be in the zip
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "initial")))
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "lambda", "handler.py")))
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "certifi")))
+            self.assertTrue(os.path.exists(os.path.join(assertion_directory, "pytz")))
+            # This shouldn't be in the zip (exclude list)
+            self.assertFalse(os.path.exists(os.path.join(assertion_directory, "initial", "test.txt")))
